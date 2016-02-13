cde1b2bd04081a3b4a93096b50e480404136e42b
BuildTools <unconfigured@null.spigotmc.org>
2016-02-13 18:32:32 -0500

    Basic project structure

---
+++ src/main/java/com/colonelhedgehog/terrashield/core/TerraShield.java
@@ -0,0 +1,24 @@
+package com.colonelhedgehog.terrashield.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * TerraShield
+ * Created by ColonelHedgehog on 2/13/16.
+ */
+public class TerraShield extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        registerEvents();
+    }
+
+    private void registerEvents()
+    {
+    }
+
+    private void registerCommands()
+    {
+    }
+}
